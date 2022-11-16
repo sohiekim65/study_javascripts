b03163c18deac5397c71a014d33a1bbdb6daddeb
sohiekim65 <sohiekim65@gmail.com>
2022-11-16 16:01:54 +0900

    commit

--- docs/vanilla_js/practice/functions_sums.js
+++ docs/vanilla_js/practice/functions_sums.js
@@ -1,14 +1,14 @@
 // 문제
 // 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성
 
-function sum(inputs) {
+let nums = [1, 2, 3, 4, 5];
+let sum_function = sum(nums);
+
+function sum(nums) {
   let sum = 0;
-  for (let input of inputs) {
-    sum += input;
+  for (let num of nums) {
+    sum += num;
   }
   return sum;
 }
-
-let num = [1, 2, 3, 4, 5];
-let sum_function = sum(num);
 console.log(sum_function);
