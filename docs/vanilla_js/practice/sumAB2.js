52213ce394b73b90b7d2232500aec8c6d1f7e913
sohiekim65 <sohiekim65@gmail.com>
2022-11-18 10:54:14 +0900

    nodejs survey commit

--- docs/vanilla_js/practice/sumAB2.js
+++ docs/vanilla_js/practice/sumAB2.js
@@ -1,20 +1,23 @@
 // 문제
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성
-// (0 < A, B < 10)
+// 첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다. 다음 T줄에는 각각 두 정수 A와 B가 주어진다.
+// A와 B는 1 이상, 1,000 이하
+// 시간초과 주의하기
 
 // 입력
-// 두 정수를 입력받아 마지막줄 0을 제외한 각 줄의 합을 구하기
+// 카운트를 먼저 입력받고 그 카운트 횟수 만큼 두 정수를 입력받아
+// 각 줄의 두 정수의 합을 구하기
 
 // 주요 단어 영문 이름 선정
 //  입력 - input
 //  출력 - output
 
 // 테스트 케이스
-// 1 1 -> 2
-// 2 3 -> 5
-// 3 4 -> 7
-// 9 8 -> 17
-// 5 2 -> 7
+// 5            --> 2
+// 1 1              46
+// 12 34            505
+// 5 500            100
+// 40 60            2000
+// 1000 1000
 
 // 입력
 const fs = require("fs");
@@ -22,8 +25,12 @@ const filepath =
   process.platform === "linux" ? "/dev/stdin" : "docs/vanilla_js/input.txt";
 let inputs = fs.readFileSync(filepath).toString().trim().split("\n");
 
-for (let i = 0; i < inputs.length; i++) {
-  let first = parseInt(inputs[i].split(" ")[0]);
-  let second = parseInt(inputs[i].split(" ")[1]);
-  console.log(first + second);
+let count = parseInt(inputs[0]);
+let result = "";
+
+for (let i = 0; i < count; i++) {
+  let A = parseInt(inputs[i + 1].split(" ")[0]);
+  let B = parseInt(inputs[i + 1].split(" ")[1]);
+  result += A + B + "\n";
 }
+console.log(result);
--- docs/vanilla_js/toy_project/survey.js
+++ docs/vanilla_js/toy_project/survey.js
@@ -8,22 +8,27 @@ let questions = [
   {
     questions_uid: "Q1",
     question: "1. 해당 매장을 방문시 매장은 청결 하였습니까?",
+    answer: "E1 E2 E3",
   },
   {
     questions_uid: "Q2",
     question: "2. 주문시 직원은 고객님께 친절 하였습니까?",
+    answer: "E1 E2 E3 E4",
   },
   {
     questions_uid: "Q3",
     question: "3. 주문하신 음료가 나오기까지 시간이 적당하였습니까?",
+    answer: "E1 E2",
   },
   {
     questions_uid: "Q4",
     question: "4. 직원이 제조한 음료에 대해 맛은 좋았습니까?",
+    answer: "E1 E2 E3 E4 E5",
   },
   {
     questions_uid: "Q5",
     question: "5. 해당 매장을 다음에도 재방문 하실 의향이 있으십니까?",
+    answer: "E1 E2 E3",
   },
 ];
 
@@ -42,34 +47,31 @@ function questions_function(i) {
 }
 
 // 답항 function
-function answer_function(i) {
-  if (questions["questions_uid"] == "Q1") {
-    for (let i = 0; i < 3; i++) {
-      console.log(`${answers[i]["example"]}`);
-    }
-  } else if (questions["questions_uid"] == "Q2") {
-    for (let i = 0; i < 4; i++) {
-      console.log(`${answers[i]["example"]}`);
-    }
-  } else if (questions["questions_uid"] == "Q3") {
-    for (let i = 0; i < 2; i++) {
-      console.log(`${answers[i]["example"]}`);
-    }
-  } else if (questions["questions_uid"] == "Q4") {
-    for (let i = 0; i < 5; i++) {
-      console.log(`${answers[i]["example"]}`);
-    }
-  } else {
-    for (let i = 0; i < 3; i++) {
-      console.log(`${answers[i]["example"]}`);
-    }
+function answer_function(answer) {
+  if (answer == "E1 E2") {
+    console.log("(1)전혀 아니다. (2)아니다.");
+  } else if (answer == "E1 E2 E3") {
+    console.log("(1)전혀 아니다. (2)아니다. (3)보통이다.");
+  } else if (answer == "E1 E2 E3 E4") {
+    console.log("(1)전혀 아니다. (2)아니다. (3)보통이다. (4)그렇다.");
+  } else if (answer == "E1 E2 E3 E4 E5") {
+    console.log(
+      "(1)전혀 아니다. (2)아니다. (3)보통이다. (4)그렇다. (5)매우 그렇다."
+    );
   }
+}
+
+// 설문자 결과 조회 function
+function result_function(i) {
+  let answers_num = inputs[i] - 1;
+  console.log(`${answers[answers_num]["example"]}`);
+  console.log(" ");
 }
 
 // 설문에 맞는 답항 출력
 for (let i = 0; i < questions.length; i++) {
   questions_function(i);
-  answer_function(i);
+  answer_function(questions[i]["answer"]);
   console.log(" ");
   console.log(`답) (${inputs[i]})`);
   console.log(" ");
@@ -80,9 +82,7 @@ console.log("------------------ 설문자 선택 ------------------ ");
 // 설문자 선택 조회
 for (let i = 0; i < questions.length; i++) {
   questions_function(i);
-  let answers_num = inputs[i] - 1;
-  console.log(`${answers[answers_num]["example"]}`);
-  console.log(" ");
+  result_function(i);
 }
 
 console.log("이용해주셔서 감사합니다!");
