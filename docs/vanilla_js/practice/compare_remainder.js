2c83feab8a316565c559eae3001355b09f314ce6
sohiekim65 <sohiekim65@gmail.com>
2022-11-19 16:15:50 +0900

    nodejs commit

--- docs/vanilla_js/practice/compare_remainder.js
+++ docs/vanilla_js/practice/compare_remainder.js
@@ -34,11 +34,16 @@ let inputs = fs
   .map(Number);
 
 let num = [];
-let count = 0;
 
+// 입력받은 숫자들을 42로 나눈 나머지값 변수에 담음
 for (let i = 0; i < 10; i++) {
   num[i] = inputs[i] % 42;
 }
 
+// 중복 숫자 제거 -> 중복되는 숫자의 인덱스를 더 작은 인덱스로 리턴
+// 나머지 값인 39, 40, 41, 0, 1, 2, 40, 41, 0, 1에서 중복되는 1의 인덱스값은
+// 마지막 인덱스 값인 9가 아닌 중복되는 인덱스의 가장 작은 값인 4가 리턴
 let compare = num.filter((A, index) => num.indexOf(A) === index);
+
+// 출력
 console.log(compare.length);
